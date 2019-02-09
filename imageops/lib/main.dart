0297473cd6752735e660e61779dc33f1190711f7
Sagun Raj Lage <sagsraj@yahoo.com>
2019-02-09 18:11:37 +0545

    Image ops

---
+++ imageops/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false,home: ImageApp()));
+
+class ImageApp extends StatefulWidget {
+  _ImageAppState createState() => _ImageAppState();
+}
+
+class _ImageAppState extends State<ImageApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(title: Text("Image App")),
+       body: Container(
+         child: Column(
+           children: <Widget>[
+             Image.asset('img/img.jpg'),
+             Text("Image"),
+           ],
+         ),
+       ),
+    );
+  }
+}
